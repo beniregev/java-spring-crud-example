7c8b43719c53068cd6db34048965ab99ebe930d8
binyamin.regev <beniregev@gmail.com>
2022-03-01 17:40:03 +0200

    Initial Commit

---
+++ src/main/java/com/beniregev/javaspringcrudexample/JavaSpringCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.javaspringcrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCrudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringCrudExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beniregev/javaspringcrudexample/JavaSpringCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.javaspringcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
